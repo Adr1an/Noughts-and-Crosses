0f2af2f4f3cddd02858884390f7a7ee717167e85
Adrian <adrian.brink@gmx.net>
2013-04-13 18:19:34 +0100

    Initial Commit

---
+++ Noughts and Crosses/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Noughts and Crosses
+//
+//  Created by Adrian Brink on 13/04/2013.
+//  Copyright (c) 2013 Adrian Brink. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
